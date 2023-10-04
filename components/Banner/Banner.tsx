66ed4c7906b521aca864a6123d50a8be058d5d2c
checkkick <tuskancik226@gmail.com>
2023-10-04 17:31:09 +0300

    add svg to background

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './banner.module.scss';
-
 export function Banner() {
   return (
     <section className={styles.wrapper}>
       <div className="container">
-        <h1 className={styles.title}>TRIUMF</h1>
-        <h2 className={styles.subtitle}>Ваша возможность сделать свой бизнес высокотехнологичным</h2>
-        <button className={styles.btn}>Связаться с нами</button>
+        <div className={styles.content}>
+          <h1 className={styles.title}>TRIUMF</h1>
+          <h2 className={styles.subtitle}>Ваша возможность сделать свой бизнес высокотехнологичным</h2>
+          <button className={styles.btn}>Связаться с нами</button>
+        </div>
       </div>
     </section>
   );
