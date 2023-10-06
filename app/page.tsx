d70b43376fbc1a33dc0ba54f636661f3ba765ddf
checkkick <tuskancik226@gmail.com>
2023-10-06 11:14:57 +0300

    add about block

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.scss'
 import { Banner } from '@/components/Banner'
+import { About } from '@/components/About'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Banner />
+      <About />
     </main>
   )
 }
