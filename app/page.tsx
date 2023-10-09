b2af70e72f788a3476812569b891b152c989298b
checkkick <tuskancik226@gmail.com>
2023-10-09 10:52:28 +0300

    company block complete

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 import styles from './page.module.scss'
 import { Banner } from '@/components/Banner'
 import { About } from '@/components/About'
+import { Company } from '@/components/Company'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Banner />
       <About />
+      <Company />
     </main>
   )
 }
---
+++ components/Company/Company.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './company.module.scss';
+import { DownloadButton } from './DownloadButton';
+
+export function Company() {
+  return (
+    <div className={styles.wrapper}>
+      <div className="container">
+        <div className={styles.about}>
+          <h2 className={styles.title}>КОМПАНИЯ</h2>
+          <h3 className={styles.subtitle}>Комплексные поставки IT-оборудования напрямую от производителей</h3>
+          <p className={styles.text}>
+            Компания TRIUMF осуществляет комплексные поставки IT-оборудования на рынке IT-инфраструктуры
+            с 2015 года, все это время учитывая быстроразвивающийся IT-сектор и потребности клиентов.
+          </p>
+          <p className={styles.subtext}>
+            Помимо оборудования TRIUMF предоставит для Ваc современный и безопасный софт, а также патчи и обновления,
+            которые будут поддерживать в актуальном и безопасном состоянии программное обеспечение.
+          </p>
+
+          <DownloadButton />
+        </div>
+      </div>
+    </div>
+  );
+}
