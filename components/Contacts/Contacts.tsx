6f3614f5e730b7e8f07cd85754c562074ca24cd5
checkkick <tuskancik226@gmail.com>
2023-10-10 17:29:21 +0300

    add qr for contacts

--- components/Contacts/Contacts.tsx
+++ components/Contacts/Contacts.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import styles from './contacts.module.scss';
 import { ContactsForm } from './ContactsForm';
@@ -11,6 +12,11 @@ export function Contacts() {
             <h2 className={styles.title}>СВЯЖИТЕСЬ С НАМИ</h2>
 
             <ContactsForm />
+
+            <div className={styles.qr}>
+              <p className={styles.qrText}>Если у вас возникли вопросы, отсканируйте QR-код, чтобы обратиться в техподдержку</p>
+              <img className={styles.qrImage} src="/contacts-qr.png" alt="contacts qr" />
+            </div>
           </div>
         </div>
       </div>
