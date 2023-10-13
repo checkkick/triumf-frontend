f38d247e205c5aade6c8df0534352bfca30b6334
checkkick <tuskancik226@gmail.com>
2023-10-13 11:21:07 +0300

    complete equipment animation

--- components/Equipment/Equipment.tsx
+++ components/Equipment/Equipment.tsx
@@ -7,15 +7,21 @@ export function Equipment() {
   const exapmle = [
     {
       image: '/equipment-1.png',
-      title: 'Lenovo ThinkSystem SN550'
+      title: 'Lenovo ThinkSystem SN550',
+      text: 'Новейший двухпроцессорный сервер, который занимает почетное место среди самых надежных серверов мира (x86)‡. Являясь новейшим структурным элементом в семействе Flex System, SN550 V2 оптимизирован для обеспечения высокой производительности, эффективности и безопасности.',
+      price: '3 000 000 руб.'
     },
     {
       image: '/equipment-2.png',
-      title: 'TP-Link JetStream T1600G-52PS'
+      title: 'TP-Link JetStream T1600G-52PS',
+      text: 'Новейший двухпроцессорный сервер, который занимает почетное место среди самых надежных серверов мира (x86)‡. Являясь новейшим структурным элементом в семействе Flex System, SN550 V2 оптимизирован для обеспечения высокой производительности, эффективности и безопасности.',
+      price: '2 000 000 руб.'
     },
     {
       image: '/equipment-3.png',
-      title: 'Dell EMC Unity 350F'
+      title: 'Dell EMC Unity 350F',
+      text: 'Новейший двухпроцессорный сервер, который занимает почетное место среди самых надежных серверов мира (x86)‡. Являясь новейшим структурным элементом в семействе Flex System, SN550 V2 оптимизирован для обеспечения высокой производительности, эффективности и безопасности.',
+      price: '4 000 000 руб.'
     },
   ]
 
@@ -27,7 +33,8 @@ export function Equipment() {
           <h2 className={styles.subtitle}>Доставка оборудования в течение 24 часов</h2>
 
           <ul className={styles.list}>
-            {exapmle.map((item, index) => <EquipmentItem image={item.image} title={item.title} key={index} />)}
+            {exapmle.map((item, index) =>
+              <EquipmentItem image={item.image} title={item.title} text={item.text} price={item.price} key={index} />)}
           </ul>
 
           <ButtonMore />
--- components/Equipment/EquipmentItem/EquipmentItem.tsx
+++ components/Equipment/EquipmentItem/EquipmentItem.tsx
@@ -6,13 +6,17 @@ import Image from 'next/image';
 interface IEquipmentItem {
   image: string
   title: string
+  text: string
+  price: string
 }
 
-export function EquipmentItem({ image, title }: IEquipmentItem) {
+export function EquipmentItem({ image, title, text, price }: IEquipmentItem) {
   return (
     <li className={styles.item}>
       <img className={styles.image} src={image} alt={title} />
       <h4 className={styles.title}>{title}</h4>
+      <p className={styles.text}>{text}</p>
+      <p className={styles.price}>{price}</p>
     </li>
   );
 }
