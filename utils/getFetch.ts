314b1005e0d7a220a9caffc8329f8fc0cad4026a
checkkick <tuskancik226@gmail.com>
2023-10-31 16:07:59 +0300

    change revalidate to api to 1m

--- utils/getFetch.ts
+++ utils/getFetch.ts
@@ -2,9 +2,9 @@ export async function getFetch(url: string) {
    const res = await fetch(`${process.env.NEXT_PUBLIC_API}/${url}/`, {
       method: 'GET',
       headers: {
-         "Content-Type": "application/json",
+         "Content-Type": "application/json"
       },
-      next: { revalidate: 10 }
+      next: { revalidate: 60 }
    })
 
    if (!res.ok) {
